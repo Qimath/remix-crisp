e08049a4bc349cf616746f8f0e422ff539e26f50
Qimath <vibert.christopher@gmail.com>
2023-11-08 12:13:21 +0100

    release

--- app/root.tsx
+++ app/root.tsx
@@ -9,11 +9,19 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import { useEffect } from "react";
+import { Crisp } from "crisp-sdk-web";
+
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
+
+useEffect(() => {
+  Crisp.configure("041dd830-e3c9-492a-9e71-8803dc102ae9");
+})
+
   return (
     <html lang="en">
       <head>
